919ae6b455dab3934cbc70742672fcccc2a30a77
Lorenzo Delana <oss.devel@searchathing.com>
2017-01-17 22:17:55 +0100

    ssh integrated in base image

--- build_all.sh
+++ build_all.sh
@@ -7,12 +7,10 @@ executing_dir()
 
 exdir=`executing_dir`
 
-cd $exdir
-./checkout_sources.sh
-
 cd $exdir/base && ./build.sh && \
 cd $exdir/mono && ./build.sh && \
 cd $exdir/develbase && ./build.sh && \
 cd $exdir/devel && ./build.sh && \
+cd $exdir && ./checkout_sources.sh && \
 cd $exdir/searchathing && ./build.sh
 

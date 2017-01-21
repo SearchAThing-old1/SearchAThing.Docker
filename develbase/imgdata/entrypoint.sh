f99c384b082621914b7b3eba0257ca398d259104
Lorenzo Delana <oss.devel@searchathing.com>
2017-01-21 18:48:49 +0100

    csharp nano syntax highlight

--- develbase/imgdata/entrypoint.sh
+++ develbase/imgdata/entrypoint.sh
@@ -2,6 +2,24 @@
 
 # one time only actions
 if [ ! -e /var/lib/docker-entrypoint/.entrypoint-develbase-executed ]; then
+	# nano editor settings
+	cat >> ~/.nanorc << EOL
+set const
+syntax "C# source" "\.cs$"
+color green "\<(bool|byte|sbyte|char|decimal|double|float|int|uint|long|ulong|new|object|short|ushort|string|base|this|void)\>"
+color brightcyan "\<(as|break|case|catch|checked|continue|default|do|else|finally|fixed|for|foreach|goto|if|is|lock|return|switch|throw|try|unchecked|while)\>"
+color cyan "\<(var|abstract|class|const|delegate|enum|event|explicit|extern|implicit|in|internal|interface|namespace|operator|out|override|params|private|protected|public|readonly|ref|sealed|sizeof|static|struct|typeof|using|virtual|volatile)\>"
+color brightwhite ""[^\"]*""
+color yellow "\<(true|false|null)\>"
+color brightblack "//.*"
+color brightblack start="/\*" end="\*/"
+color brightblue start="/\*\*" end="\*/"
+color brightgreen,green " +$"
+EOL
+
+	cd /usr/share/nano
+	for i in `\ls`; do echo "include /usr/share/nano/$i" >> ~/.nanorc; done
+
 	touch /var/lib/docker-entrypoint/.entrypoint-develbase-executed
 fi
 

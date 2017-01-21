5a3549b97d7837f5413120fdb8fe301f7997ed70
Lorenzo Delana <oss.devel@searchathing.com>
2017-01-21 15:01:46 +0100

    optional clone src and build searchathing container

--- build_all.sh
+++ build_all.sh
@@ -10,7 +10,8 @@ exdir=`executing_dir`
 cd $exdir/base && ./build.sh && \
 cd $exdir/mono && ./build.sh && \
 cd $exdir/develbase && ./build.sh && \
-cd $exdir/devel && ./build.sh && \
-cd $exdir && ./checkout_sources.sh && \
-cd $exdir/searchathing && ./build.sh
+cd $exdir/devel && ./build.sh
+
+#cd $exdir && ./checkout_sources.sh && \
+#cd $exdir/searchathing && ./build.sh
 

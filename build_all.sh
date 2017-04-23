ba4edee7edac32370466dba3a5bf41f3112d45ec
Lorenzo Delana <l.delana@swsglobal.com>
2017-04-23 20:33:06 +0200

    removed monodevelop

--- build_all.sh
+++ build_all.sh
@@ -7,10 +7,10 @@ executing_dir()
 
 exdir=`executing_dir`
 
-cd $exdir/base && ./build.sh && \
-cd $exdir/mono && ./build.sh && \
-cd $exdir/develbase && ./build.sh && \
-cd $exdir/devel && ./build.sh
+cd $exdir/base && ./build.sh $@ && \
+cd $exdir/mono && ./build.sh $@ && \
+cd $exdir/develbase && ./build.sh $@ && \
+cd $exdir/devel && ./build.sh $@
 
 
 #cd $exdir && ./checkout_sources.sh && \
